9dc6a241cabdcbfb7a1d6197c7792d63ae99d2af
TalibMalik7 <mohammedtalib5@gmail.com>
2023-02-11 16:10:45 +0530

    Third

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,11 @@ public class Boat implements WaterVehicle {
     private String name;
     private int capacity;
 
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -11,6 +16,7 @@ public class Boat implements WaterVehicle {
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
+
 
     @Override
     public String getVehicleName() {
